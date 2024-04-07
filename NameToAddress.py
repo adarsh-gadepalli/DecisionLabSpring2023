35a8830bdce315c2f6bf10115d73dc0f3ca25488
ShammasAhmed <107204379+ShammasAhmed@users.noreply.github.com>
2024-04-07 13:48:53 -0500

    Adding NameToAddress
    This file creates a dictionary of addresses given a list of possible "points of interest" a user wants to visit. Each entry generates up to 6 addresses, allowing the user to choose the one they're looking for.

---
+++ NameToAddress.py
@@ -0,0 +1,55 @@
+import requests
+
+# Define a list of keywords
+# Need to get this directly from the user input
+keywords = ["Rice University",
+            "POST Houston",
+            "NRG Stadium"]
+
+# Define the API endpoint URL
+url = "https://dev.virtualearth.net/REST/v1/Autosuggest"
+
+#Starting a for-loop for all locations
+for point_of_interest in keywords:
+
+    # Create an empty list that will display all addresses found
+    addresses = {}
+
+    # Get user input for the query
+    query = point_of_interest
+
+    # Define query parameters
+    params = {
+        "query": query,
+        "includeEntityTypes": "Business,Address",
+        "key": "AmSsyd6lFRUcPk43ICNzM_25im4fEI5qjjlg54c9l4Xi3lx5ys5sDVBlx98-4el0"
+    }
+
+    # Make the GET request
+    response = requests.get(url, params=params)
+
+
+    # Check if the request was successful
+    if response.status_code == 200:
+        # Parse JSON response
+        data = response.json()
+
+        # Extract name and formatted address
+        for result in data['resourceSets'][0]['resources'][0]['value']:
+            if 'name' in result:
+                name = result['name']
+                addresses[name] = "No Address"
+            #   print("Name:", name)
+            if 'address' in result:
+                curr_address = result['address']['formattedAddress']
+                addresses[name] = curr_address
+                # print("Address:", name)
+
+            else:
+                name = "Unknown entity type"
+
+            print()
+        print(f"Your entry: {point_of_interest}")
+        print(f"Possible Addresses: {addresses}")
+    else:
+        print("Failed to retrieve data. Status code:", response.status_code)
